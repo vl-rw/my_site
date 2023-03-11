bd2d89c08a0386c1eccd19e47f0e89999b079b6d
vl-rw <124644489+vl-rw@users.noreply.github.com>
2023-03-11 15:37:04 +1000

    Add files via upload

---
+++ politics/json.js
@@ -0,0 +1,10 @@
+json = {
+    
+    
+"img0" : "img/comm_starve.jpg", "img1" : "img/debate.png", "img2" : "img/hghmgmh.png", "img3" : "img/hippocrates.jpg", "img4" : "img/inc1.png", "img5" : "img/inc2.jpg", "img6" : "img/jtyjty.png", "img7" : "img/no-soc.png", "img8" : "img/photo_2021-01-22_17-51-27.jpg", "img9" : "img/reigan.jpg", "img10" : "img/rus_free.jpg", "img11" : "img/tkyktyt.png"
+
+
+}; 
+
+
+
